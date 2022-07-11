427283cd9ca93b45a080ea82f45758b4b2578ac7
Tina Kovalova <kovalevatkv@gmail.com>
2022-07-11 14:25:28 +0300

    hw15.0

---
+++ my-app/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {ANAKIN_IMAGE, RAY_IMAGE} from "./constatnts";
+import Post from './Components/Post.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Post author={{
+            name: "Anakin skywalker",
+            photo: ANAKIN_IMAGE,
+            nickname: "@dart_vader"
+        }}
+              content='WTF? Who is Ray? Why she is Skywalker? Luke...?'
+              image={RAY_IMAGE}
+              date={"26 февр."}/>
+    </React.StrictMode>
+);
+

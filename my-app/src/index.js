8c2504c3bb66a88513ac4ac82a45fc8f355e8f6c
Tina Kovalova <kovalevatkv@gmail.com>
2022-07-11 20:00:40 +0300

    renamed: Post.js to .jsx,  constants file

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import {ANAKIN_IMAGE, RAY_IMAGE} from "./constatnts";
-import Post from './Components/Post.js';
+import './Styles/index.css';
+import {ANAKIN_IMAGE, RAY_IMAGE} from "./constants";
+import Post from './Components/Post.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
